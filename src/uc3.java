32ea3944bcb5fb5cd582219d96ee90cf9d07659c
sravan <sravan.k.vodnala@gmail.com>
2022-05-23 21:25:28 +0530

    Initial commit

---
+++ src/uc3.java
@@ -0,0 +1,13 @@
+public class uc3 {
+    public static void main(String[] args){
+        int wageperhour=20;
+        int halfdayhour=4;
+        int dailywage;
+         String display="Employee is present part time--";
+         dailywage=(wageperhour*halfdayhour);
+         System.out.println(display+" "+dailywage);
+
+         
+    }
+
+}
